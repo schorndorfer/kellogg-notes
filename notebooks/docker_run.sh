272423f11b4749739bc81f0165cdd50ede68731c
WKT <will.k.t@gmail.com>
2019-02-17 11:05:07 -0600

    slide updates

--- notebooks/docker_run.sh
+++ notebooks/docker_run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+export NLP_NOTEBOOKS=/Users/WKT/Code/schorndorfer/kellogg-notes/notebooks
 docker run -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -it -v ${NLP_NOTEBOOKS}:/home/jovyan/work/notebooks --rm schorndorfer/nlp:latest
 
