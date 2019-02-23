6d115695d104e5116541c770db3173028ed869c9
William Thompson <will.k.t@gmail.com>
2019-02-23 09:28:53 -0600

    Update docker_run.sh

--- notebooks/docker_run.sh
+++ notebooks/docker_run.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-export NLP_NOTEBOOKS=/Users/${USER}/Code/schorndorfer/kellogg-notes/notebooks
 docker run -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -it -v ${NLP_NOTEBOOKS}:/home/jovyan/work/notebooks --rm schorndorfer/nlp:latest
 
